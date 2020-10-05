847355a0cdf8c4ab818cd4c1044114079ad586d5
Sean Tarzy <seantarzy@gmail.com>
2020-10-05 19:01:51 -0400

    need to create songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,17 +1,29 @@
 class Song < ApplicationRecord
      belongs_to :artist
 
-     def self.seed_songs(artist_id)
-         page_number=1
-        page_cap = 10
+    def self.seed_songs(artist_id)
+        page_number=1
+        page_cap = 2
         while(page_number <= page_cap && !!page_number) do
             response = RestClient.get("https://genius.com/api/artists/#{artist_id}/songs?page=#{page_number}&sort=popularity")
             response = JSON.parse(response)
             songs = response['response']['songs']
-            songs.each do |song|
-                print song
-            end
+            self.create_songs(songs, artist_id)
             page_number = response['response']['next_page']
         end
-     end
+    end
+    def self.create_songs(songs, artist_id)
+        songs.each do |song|
+            song_url = song['url']
+            lyrics = self.get_lyrics(song_url)
+           x = Song.create(full_title: song['full_title'], genius_id: song['id'], lyrics: lyrics, artist_id: artist_id, url: song['url'], image: song["song_art_image_url"], title: song['title'])
+            byebug
+        end
+    end
+
+    def self.get_lyrics(song_url)
+      response =RestClient.get(song_url)
+      parsed_data = Nokogiri::HTML.parse(response)
+      lyrics = parsed_data.css('div.lyrics').text
+    end
 end
