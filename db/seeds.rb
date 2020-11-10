32896820e3f58a3ea7dcd2e3c2bb43dd9ad11c43
jackmt9 <jackmt9@gmail.com>
2020-11-10 17:10:25 -0500

    seed data altered

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
 artists = ['The Beatles', 'Eric Clapton']
 Artist.seed_artist_and_songs(artists)
+
 puts "Creating User --- email: admin@gmail.com, password: admin"
-User.create(first_name: 'admin', last_name: 'admin', email: 'admin@gmail.com', password: 'admin')
+user = User.create(first_name: 'admin', last_name: 'admin', email: 'admin@gmail.com', password: 'admin')
+
+puts "Creating Playlists"
+playlists = ['Chemistry', 'Biology', 'Astronomy']
+playlists.each do |title|
+    playlist = Playlist.create(title: title, user: user)
+    puts "Playlist #{title} created"
+
+    # Populate playlists with bookmarks here
+end
+
 puts "--- SEEDING COMPLETE ---"
