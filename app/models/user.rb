e74b53b1c8fd2d89f10e4dba88b0af402ab1b816
jackmt9 <jackmt9@gmail.com>
2020-11-10 16:43:43 -0500

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_many :playlists
     has_secure_password
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
 end
