67160030988ea5a1b1d7b3b452ded3c5ba8822fe
jackmt9 <jackmt9@gmail.com>
2020-11-12 14:25:14 -0500

    refactor

--- app/controllers/add_youtube_i_dto_bookmarks_controller.rb
+++
@@ -1,2 +0,0 @@
-class AddYoutubeIDtoBookmarksController < ApplicationController
-end
--- test/controllers/add_youtube_i_dto_bookmarks_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AddYoutubeIDtoBookmarksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
